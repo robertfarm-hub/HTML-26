27059dab4e3f3f506b32f2bd99d5c2a0722ab597
robertfarm-hub <setaroope@gmail.com>
2026-09-14 17:46:05 +0300

    fix: correct floor height for level 1 in game settings

--- odfm-sivusto/js/peli.js
+++ odfm-sivusto/js/peli.js
@@ -9,7 +9,7 @@ const LUUKKU_LEVEYS = 12;
 
 const KENTAT = [
     { kuva: "kentta-0.png", luukku: 88, lattia: 9.4 },
-    { kuva: "kentta-1.png", luukku: 12, lattia: 19.4 }
+    { kuva: "kentta-1.png", luukku: 12, lattia: 18.8 }
 ];
 
 let kerros = 0;
