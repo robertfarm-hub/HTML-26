d9739077bf6b47f24de46d55edb82391a1b63a3b
robertfarm-hub <setaroope@gmail.com>
2026-09-14 14:41:37 +0300

    hahmo testi

--- odfm-sivusto/js/peli.js
+++ odfm-sivusto/js/peli.js
@@ -2,8 +2,18 @@ const kentta = document.getElementById("kentta");
 const pelaaja = document.getElementById("pelaaja");
 
 const NOPEUS = 0.4;
+const RUUTUVALI = 180;
+
 let x = 50;
 let suunta = 0;
+let katse = 1;
+let pelialkanut = false;
+let ruutu = 0;
+let viimeVaihto = 0;
+
+function vaihdaKuva(nimi) {
+    pelaaja.style.backgroundImage = 'url("../images/' + nimi + '.png")';
+}
 
 document.addEventListener("keydown", (tapahtuma) => {
     if (tapahtuma.key === "ArrowLeft") suunta = -1;
@@ -24,14 +34,34 @@ window.addEventListener("pointerup", () => {
     suunta = 0;
 });
 
-function paivita() {
+function paivita(aika) {
+    if (suunta !== 0) {
+        if (!pelialkanut) {
+            pelialkanut = true;
+            vaihdaKuva("dante-lyhty-a");
+        }
+
+        katse = suunta;
+
+        if (aika - viimeVaihto > RUUTUVALI) {
+            viimeVaihto = aika;
+            ruutu = 1 - ruutu;
+            vaihdaKuva(ruutu === 0 ? "dante-lyhty-a" : "dante-lyhty-b");
+        }
+    } else if (pelialkanut && ruutu !== 0) {
+        ruutu = 0;
+        vaihdaKuva("dante-lyhty-a");
+    }
+
     x = x + suunta * NOPEUS;
 
     if (x < 4.3) x = 4.3;
     if (x > 95.7) x = 95.7;
 
     pelaaja.style.left = x + "%";
+    pelaaja.style.transform = "translateX(-50%) scaleX(" + katse + ")";
+
     requestAnimationFrame(paivita);
 }
 
-paivita();
+requestAnimationFrame(paivita);
