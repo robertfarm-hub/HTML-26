1ef0d6f9583fc1064fe83b065df39abb7dcb6e08
robertfarm-hub <setaroope@gmail.com>
2026-09-15 11:24:34 +0300

    alkukenttä muutettu

--- odfm-sivusto/js/peli.js
+++ odfm-sivusto/js/peli.js
@@ -15,7 +15,7 @@ const LYHTY_X = 0.855;
 const LYHTY_Y = 0.533;
 
 const KENTAT = [
-    { kuva: "kentta-0.png", luukku: 88, lattia: 9.4 },
+    { kuva: "kentta-0.png", luukku: 80, lattia: 10.6 },
     { kuva: "kentta-1.png", luukku: 12, lattia: 18.8 },
     { kuva: "kentta-2.png", luukku: 88, lattia: 14.4 },
     { kuva: "kentta-3.png", luukku: 12, lattia: 11.9 },
